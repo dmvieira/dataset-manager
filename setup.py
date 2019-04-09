a572403e2a0c099b7abdf0114406ab0519aa6821
Diogo Munaro <diogo.munaro@corp.globo.com>
2019-04-09 01:06:27 -0300

    Long description

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@
 
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="dataset_manager",
     version="0.0.2",
     description="Manage and automatize datasets for data science projects.",
+    long_description=long_description,
     author="Diogo Munaro Vieira",
     author_email="diogo.mvieira@gmail.com",
     license='Apache 2',
