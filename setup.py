5eba2cbc0ea5f344d5f11dd5c2e068bb9e4d1ed8
Diogo Munaro <diogo.mvieira@gmail.com>
2019-04-09 14:20:42 -0300

    Bump 0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="dataset_manager",
-    version="0.0.4",
+    version="0.0.5",
     description="Manage and automatize datasets for data science projects.",
     long_description=long_description,
     long_description_content_type='text/markdown',
