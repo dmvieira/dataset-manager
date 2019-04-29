1d191546a6b7ae48e60de8b81918a60ee29b9a48
Susana Bouchardet <susana.bouchardet@gmail.com>
2019-04-29 20:07:55 -0300

    increase version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name="dataset_manager",
-    version="0.0.10",
+    version="0.0.11",
     description="Manage and automatize datasets for data science projects.",
     long_description=long_description,
     long_description_content_type='text/markdown',
